a14825405b9cad49b4c6d53196d9410f15488a70
Piscesilent <Piscesilent@users.noreply.github.com>
2016-12-15 13:42:14 +0800

    change to springboot 1.4.2 & change log4j to log4j2

--- src/main/java/timer/Application.java
+++ src/main/java/timer/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-import timer.utils.PropUtils;
-
 /**
  * Application bootstrap
  * @author SilenT
@@ -15,32 +13,12 @@ import timer.utils.PropUtils;
 public class Application {
 
 	/**
-	 * spring boot properties
-	 */
-	private static final String SPRING_BOOT_PROP = "application.properties";
-
-	/**
-	 * quartz properties
-	 */
-	private static final String QUARTZ_PROP = "quartz.properties";
-
-	/**
-	 * log4j properties
-	 */
-	private static final String LOG4J_PROP = "log4j.properties";
-
-	/**
 	 * main method
 	 * @author SilenT
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		
-		/**
-		 * init props
-		 */
-		PropUtils.initProperties(SPRING_BOOT_PROP, QUARTZ_PROP, LOG4J_PROP);
 		
 		/**
 		 * running serive
